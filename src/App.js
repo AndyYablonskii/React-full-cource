4cefd0d7cc44b885d7eb930267fed1941d894891
andyyablonskii <andyyablonskii@gmail.com>
2023-03-09 11:27:22 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import Navbar from "./navbar";
 
 function App() {
-  const title = 'Welcome to the new blog'
+  const title = "Welcome to the new blog";
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
+        <h1>{title}</h1>
+        <Navbar />
       </div>
     </div>
   );
---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+    <nav className="Navbar">
+        <h1>The to do list</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">Create a new task</a>
+        </div>
+    </nav> 
+    );
+}
+
+export default Navbar;
