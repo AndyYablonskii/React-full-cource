3e59cd5d93661f013731a65f39b637a9f203ff00
andyyablonskii <andyyablonskii@gmail.com>
2023-03-12 09:46:23 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
+
+
   const title = "Welcome to the new blog";
 
   return (
     <div className="App">
+      <p>{title}</p>
       <Navbar />
       <div className="content">
         <Home />
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
-import { click } from "@testing-library/user-event/dist/click";
 import { useState } from "react";
-
 const Home = () => {
-const [count, setCount] = useState(0);
+    
+    const [count, setCount] = useState(0)
 
 return (
-    <div className="contriner">
-    <button onClick={ () => setCount(count + 1) } >{count}</button>
+    <div className="home">
+        <button onClick={() => setCount(count + 1)}>
+            {count}
+        </button>
     </div>
 );
 };
